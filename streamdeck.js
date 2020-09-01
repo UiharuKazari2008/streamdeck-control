562af60253f92a28b7930b6341125f1071ad05df
Yukimi Kazari <dev@9854.moe>
2020-08-31 21:58:28 -0400

    inital testing of sdc

--- streamdeck.js
+++ streamdeck.js
@@ -17,15 +17,11 @@ if (typeof config.devices["0"] !== undefined) {
         if (config.devices[device].deviceID === "") {
             streamDeck = openStreamDeck() // No Device given? No Problem!
             deviceInfo = getStreamDeckInfo()
-
         } else {
             // Given a specific ID
             streamDeck = openStreamDeck(config.devices[device].deviceID.toString())
             deviceInfo = getStreamDeckInfo(config.devices[device].deviceID.toString())
         }
-        // Print Device Info
-        console.log(deviceInfo)
-
         // Reset keypad
         streamDeck.clearAllKeys()
 
@@ -34,20 +30,56 @@ if (typeof config.devices["0"] !== undefined) {
             streamDeck.setBrightness(config.devices[device].brightness.toString())
         }
 
+        const deviceKeys = config.devices[device].keys
+
         // Fill keys from config
-        for (let key in config.devices[device].keys) {
-            console.log(config.devices[device].keys[key])
-            streamDeck.fillColor(parseInt(key), config.devices[device].keys[key].fillParam[0], config.devices[device].keys[key].fillParam[1], config.devices[device].keys[key].fillParam[2])
+        for (let key in deviceKeys) {
+            let keySetting = deviceKeys[key]
+            if (keySetting.fillType === "color") {
+                if ((typeof keySetting.fillParam).toString() === "Array" && keySetting.fillParam.length === 3) {
+                    streamDeck.fillColor(parseInt(key), keySetting.fillParam[0], keySetting.fillParam[1], keySetting.fillParam[2])
+                } else {
+                    console.error(`Key ${key} Fill Settings are not as expected, Should be a Array ["R", "G", "B"]`)
+                }
+            } else if (keySetting.fillType === "image") {
+                if ((typeof keySetting.fillParam).toString() === "String") {
+                    //streamDeck.fillColor(parseInt(key), keySetting.fillParam[0], keySetting.fillParam[1], keySetting.fillParam[2])
+                } else {
+                    console.error(`Key ${key} Fill Settings are not as expected, Should be a String "./file/path"`)
+                }
+            } else {
+                console.error(`Unknown Fill Type of : ${keySetting.fillType}`)
+            }
+
+
         }
 
         // Press Key
         streamDeck.on('down', keyIndex => {
-            console.log('key %d down', keyIndex)
+            if (deviceKeys[keyIndex] !== undefined) {
+                console.log(`Key ${keyIndex} is bound like this...`)
+                console.log(deviceKeys[keyIndex])
+
+                if (deviceKeys[keyIndex].eventType === "get") {
+                    console.log("GET REQUEST")
+                } else if (deviceKeys[keyIndex].eventType === "post") {
+                    console.log("POST REQUEST")
+                } else if (deviceKeys[keyIndex].eventType === "open") {
+                    console.log("OPEN REQUEST")
+                } else {
+                    console.log("UNKNOWN REQUEST TYPE")
+                }
+
+            } else {
+                console.log(`Key ${keyIndex} is not bound to any key at this time.`)
+            }
         })
+
         // Release Key
-        streamDeck.on('up', keyIndex => {
+        /*streamDeck.on('up', keyIndex => {
             console.log('key %d up', keyIndex)
-        })
+        })*/
+
         // Error? What could possibly go wrong?
         streamDeck.on('error', error => {
             console.error(error)
