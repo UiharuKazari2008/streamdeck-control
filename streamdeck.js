ad70260a4c349ef8e84613c6bba37025d8f6c7ef
Yukimi Kazari <dev@9854.moe>
2020-09-01 12:34:37 -0400

    testing key drawing

--- streamdeck.js
+++ streamdeck.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const colors = require('colors');
 const request = require('request').defaults({ encoding: null });
 const { openStreamDeck, listStreamDecks, getStreamDeckInfo } = require('elgato-stream-deck');
 
 const config = require('./config.json');
 
-console.log('Devices: ', listStreamDecks());
+console.log('Devices:\n', listStreamDecks());
 
 if (typeof config.devices !== undefined) {
     for (let device in config.devices) {
@@ -15,116 +16,122 @@ if (typeof config.devices !== undefined) {
         let folderID = ''
         let keyImages = new Map();
 
+        // Initialize device object
         if (config.devices[device].deviceID === "") {
-            streamDeck = openStreamDeck() // No Device given? No Problem!
-            deviceInfo = getStreamDeckInfo()
+            streamDeck = openStreamDeck(listStreamDecks()[0].path) // No Device given? No Problem!
+            deviceInfo = getStreamDeckInfo(listStreamDecks()[0].path)
+            streamDeck.clearAllKeys()
         } else {
             // Given a specific ID
             streamDeck = openStreamDeck(config.devices[device].deviceID.toString())
             deviceInfo = getStreamDeckInfo(config.devices[device].deviceID.toString())
+            streamDeck.clearAllKeys()
         }
-        // Reset keypad
-        streamDeck.clearAllKeys()
         // Set Brightness if set
         if (typeof config.devices[device].brightness !== undefined) {
-            streamDeck.setBrightness(config.devices[device].brightness.toString())
+            streamDeck.setBrightness(parseInt(config.devices[device].brightness))
         }
 
         function sleep(millis) {
             return new Promise(resolve => setTimeout(resolve, millis));
         }
-        function cacheImages(deviceKeys) {
+        // Cache Images for Keys and Folders
+        function cacheImages() {
+            console.log("Generating Images for Cache".bgBlack.white);
+            // Generate Default Images
             if (fs.existsSync(path.resolve(__dirname, 'img/back.png'))) {
-                console.log(`Set Image for 0 to back button`)
                 sharp(path.resolve(__dirname, 'img/back.png'))
                     .flatten() // Eliminate alpha channel, if any.
                     .resize(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE) // Scale up/down to the right size, cropping if necessary.
                     .raw() // Give us uncompressed RGB.
                     .toBuffer()
                     .then(buffer => {
-                        keyImages.set(`key-back`, buffer)
+                        console.log("Caching Back Button");
+                        keyImages.set(`default-back`, buffer);
                     })
                     .catch(err => {
-                        console.error(`Failed to set image on key 0 due to a Sharp error! ....`)
-                        console.error(err)
+                        console.error(`Failed to cache for Back due to a Sharp error! ....`);
+                        console.error(err);
                     })
             } else {
-                console.error(`Set Image for 0 to back button failed, Does not Exist!`)
+                console.error(`Failed to cache image for back icon, Does not Exist!`);
             }
+
+            // Generate Per-Key Images
+            const deviceKeys = config.devices[device].keys;
             for (let index in deviceKeys) {
                 function generateCacheSet(keySetting, key, folder) {
+                    let keyIndexString
+                    if (folder !== undefined) {
+                        keyIndexString = `${folder}:${key}`
+                    } else {
+                        keyIndexString = `${key}`
+                    }
                     if (keySetting.fillType === "image") {
                         if ((typeof keySetting.fillParam).toString() === "string") {
                             if (fs.existsSync(path.resolve(__dirname, keySetting.fillParam.toString()))) {
-                                console.log(`Set Image for ${key} to ${keySetting.fillParam.toString()}`)
                                 sharp(path.resolve(__dirname, keySetting.fillParam.toString()))
                                     .flatten() // Eliminate alpha channel, if any.
                                     .resize(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE) // Scale up/down to the right size, cropping if necessary.
                                     .raw() // Give us uncompressed RGB.
                                     .toBuffer()
                                     .then(buffer => {
-                                        if (folder !== undefined) {
-                                            keyImages.set(`${folder}-${key}`, buffer)
-                                        } else {
-                                            keyImages.set(`key-${key}`, buffer)
-                                        }
+                                        keyImages.set(keyIndexString, buffer) // Save Buffer to Map
                                     })
                                     .catch(err => {
-                                        console.error(`Failed to set image cache on key ${key} due to a Sharp error! ....`)
+                                        console.error(`Failed to cache for key #${keyIndexString} due to a Sharp error! ....`)
                                         console.error(err)
                                     })
                             } else {
-                                console.error(`Failed to cache image for ${key} to ${keySetting.fillParam.toString()} failed, Does not Exist!`)
+                                console.error(`Failed to cache for key #${keyIndexString} to file "${keySetting.fillParam.toString()}", File does not exist!`)
                             }
                         } else {
-                            console.error(`Failed to cache image for ${key} : Fill Settings are not as expected, Should be a String "./file/path"`)
+                            console.error(`Failed to cache image for key #${keyIndexString}, Fill Settings are not as expected (Should be a String "./file/path")`)
                         }
-                    } else if (keySetting === "null") {
-
-                    } else {
-                        console.error(`Unknown Fill Type of : ${keySetting.fillType}`)
                     }
                 }
-                if (deviceKeys[index].type === "folder") {
+
+                if (deviceKeys[index].type === "action") { // if is standard action button
+                    generateCacheSet(deviceKeys[index], index)
+                } else if (deviceKeys[index].type === "folder") { // if is a folder of items
                     for (let folderIndex in deviceKeys[index].items) {
                         generateCacheSet(deviceKeys[index].items[folderIndex], folderIndex, index)
                     }
-                } else {
-                    generateCacheSet(deviceKeys[index], index)
                 }
-
             }
         }
-        cacheImages(config.devices[device].keys)
-        function drawKeys(deviceKeys, arrayType) {
-            streamDeck.clearAllKeys()
-            if (arrayType === 'folder') {
+        cacheImages();
+
+        function drawKeys(deviceKeys, folderIndex) {
+            streamDeck.clearAllKeys() // Erase Current Keys off screen
+            if (folderIndex !== undefined) { // If inside folder, set first item to back icon
                 streamDeck.fillImage(0, keyImages.get('key-back'))
             }
-            for (let key in deviceKeys) {
+            for (let index in deviceKeys) {
                 let keySetting
-                if (arrayType === 'folder') {
-                    keySetting = deviceKeys[key]
-                    key = parseInt(key) + 1
+                let keyIndex
+                let keyIndexString
+
+                if (folderIndex !== undefined) {
+                    keySetting = deviceKeys[index]
+                    keyIndex = parseInt(index) + 1
+                    keyIndexString = `${folderIndex}:${index}`
                 } else {
-                    keySetting = deviceKeys[key]
+                    keySetting = deviceKeys[index]
+                    keyIndex = parseInt(index)
+                    keyIndexString = `${index}`
                 }
+
                 if (keySetting.fillType === "color") {
                     if ((typeof keySetting.fillParam).toString() === "object" && keySetting.fillParam.length === 3) {
-                        console.log(`Set Color for ${key} to ${keySetting.fillParam[0]},${keySetting.fillParam[1]},${keySetting.fillParam[2]}`)
-                        streamDeck.fillColor(parseInt(key), keySetting.fillParam[0], keySetting.fillParam[1], keySetting.fillParam[2])
+                        console.log(`Set Color for key #${keyIndexString} to ${keySetting.fillParam[0]},${keySetting.fillParam[1]},${keySetting.fillParam[2]}`)
+                        streamDeck.fillColor(parseInt(keyIndex), keySetting.fillParam[0], keySetting.fillParam[1], keySetting.fillParam[2])
                     } else {
-                        console.error(`Key ${key} Fill Settings are not as expected, Should be a Array ["R", "G", "B"]`)
+                        console.error(`Failed to fill Key #${keyIndexString}, Fill Settings are not as expected (Should be a Array ["R", "G", "B"])`)
                     }
                 } else if (keySetting.fillType === "image") {
-                    if (arrayType === 'folder') {
-                        console.log(keyImages.has(`key-${key}`))
-                        streamDeck.fillImage(parseInt(key), keyImages.get(`${folderID}-${key}`))
-                    } else {
-                        console.log(keyImages.has(`key-${key}`))
-                        streamDeck.fillImage(parseInt(key), keyImages.get(`key-${key}`))
-                    }
-                } else if (deviceKeys[key] === "null") {
+                    streamDeck.fillImage(parseInt(keyIndex), keyImages.get(keyIndexString))
+                } else if (deviceKeys[keyIndex] === "null") {
                     // Skip Key cause its set to blank
                 } else {
                     console.error(`Unknown Fill Type of : ${keySetting.fillType}`)
@@ -139,12 +146,10 @@ if (typeof config.devices !== undefined) {
             console.log("Waiting to draw keys");
             await sleep(2000);
             if (typeof config.devices[device].fillDevice !== undefined) {
-                // If set to not fill, then set each key
-                if (config.devices[device].fillDevice === "") {
-                    drawKeys(config.devices[device].keys, 'init')
-                } else {
-                    // Fill Entire Panel, but not implimented yet LOL, do it your self loser! (or wait for me to add it)
-                }
+                // Fill entire device scree
+            } else {
+                // Draw Each Key
+                drawKeys(config.devices[device].keys)
             }
         }
 
@@ -154,15 +159,15 @@ if (typeof config.devices !== undefined) {
         // Press Key
         streamDeck.on('down', keyIndex => {
             if (config.devices[device].keys[keyIndex] !== undefined && config.devices[device].keys[keyIndex] !== null) {
-                console.log(`Key ${keyIndex} is bound like this...`)
-
                 function doKeyAction(deviceKeys) {
-                    let keyRealIndex = keyIndex;
+                    let keyRealIndex
+
                     if (folderID !== '') {
                         keyRealIndex = parseInt(keyIndex.toString()) - 1
+                    } else {
+                        keyRealIndex = parseInt(keyIndex.toString())
                     }
-                    console.log(keyRealIndex)
-                    console.log(deviceKeys[keyRealIndex])
+
                     if (deviceKeys[keyRealIndex].type === "action") {
                         if (deviceKeys[keyRealIndex].eventType === "get") {
                             if ((typeof deviceKeys[keyRealIndex].eventParam).toString() === "string" && deviceKeys[keyRealIndex].eventParam !== undefined) {
@@ -187,25 +192,25 @@ if (typeof config.devices !== undefined) {
                         }
                     } else if (deviceKeys[keyRealIndex].type === "folder") {
                         if (deviceKeys[keyRealIndex].items !== undefined && deviceKeys[keyRealIndex].items.length > 0) {
-                            folderID = keyRealIndex;
-                            drawKeys(deviceKeys[keyRealIndex].items, 'folder');
+                            folderID = keyIndex;
+                            drawKeys(deviceKeys[keyRealIndex].items, folderID);
                         } else {
-                            console.log(`Folder on Key ${keyRealIndex} is not correctly configured or has no items in it`)
+                            console.log(`Folder on Key #${keyRealIndex} is not correctly configured or has no items in it`)
                         }
                     }
                 }
 
                 if (folderID === "") {
-                    doKeyAction(config.devices[device].keys)
+                    doKeyAction(config.devices[device].keys);
                 } else {
                     if (keyIndex === 0) {
-                        folderID = ''
-                        drawKeys(config.devices[device].keys, 'init')
+                        folderID = '';
+                        drawKeys(config.devices[device].keys);
                     } else {
                         if (config.devices[device].keys[folderID].items !== undefined && config.devices[device].keys[folderID].items.length > 0) {
-                            doKeyAction(config.devices[device].keys[folderID].items)
+                            doKeyAction(config.devices[device].keys[folderID].items);
                         } else {
-                            console.log(`Folder on Key ${keyIndex} is not correctly configured`)
+                            console.log(`Folder #${folderID} with key #${keyIndex} is not correctly configured`);
                         }
                     }
                 }
