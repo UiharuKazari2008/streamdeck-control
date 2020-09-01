149b000660646d8b25a98a7ea6962f267807e29d
Yukimi Kazari <dev@9854.moe>
2020-08-31 21:17:03 -0400

    inital testing of sdc

--- streamdeck.js
+++ streamdeck.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { openStreamDeck, listStreamDecks } = require('elgato-stream-deck')
 
-const config = require('../config.json');
+const config = require('./config.json');
 
 console.log('Devices: ', listStreamDecks())
 
