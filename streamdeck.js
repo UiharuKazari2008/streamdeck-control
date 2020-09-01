771b56ff533836950b466c02abad78ea63f470bc
Yukimi Kazari <dev@9854.moe>
2020-09-01 00:44:03 -0400

    Added GET request support

--- streamdeck.js
+++ streamdeck.js
@@ -115,7 +115,7 @@ if (typeof config.devices["0"] !== undefined) {
                 function doKeyAction(deviceKeys) {
                     let keyRealIndex = keyIndex;
                     if (folderID !== '') {
-                        keyRealIndex = keyIndex + 1
+                        keyRealIndex = parseInt(keyIndex.toString()) + 1
                     }
                     if (deviceKeys[keyRealIndex].type === "action") {
                         if (deviceKeys[keyRealIndex].eventType === "get") {
