5bbf9effd2c2c0c069681312cbb3c4e5926210e2
Yukimi Kazari <dev@9854.moe>
2020-08-31 21:15:55 -0400

    inital testing of sdc

--- streamdeck.js
+++ streamdeck.js
@@ -1,27 +1,54 @@
 const path = require('path')
-const { openStreamDeck } = require('elgato-stream-deck')
+const { openStreamDeck, listStreamDecks } = require('elgato-stream-deck')
 
-// Automatically discovers connected Stream Decks, and attaches to the first one.
-// Throws if there are no connected stream decks.
-// You also have the option of providing the devicePath yourself as the first argument to the constructor.
-// For example: const myStreamDeck = new StreamDeck('\\\\?\\hid#vid_05f3&pid_0405&mi_00#7&56cf813&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}')
-// On linux the equivalent would be: const myStreamDeck = new StreamDeck('0001:0021:00')
-// Available devices can be found with listStreamDecks()
-const myStreamDeck = openStreamDeck()
+const config = require('../config.json');
 
-myStreamDeck.on('down', keyIndex => {
-    console.log('key %d down', keyIndex)
-})
+console.log('Devices: ', listStreamDecks())
 
-myStreamDeck.on('up', keyIndex => {
-    console.log('key %d up', keyIndex)
-})
+let streamDeck0
+let streamDeck1
+let streamDeck2
+
+if (typeof config.devices["0"] !== undefined) {
+    if (typeof config.devices["0"].deviceID !== undefined) {
+        if (config.devices["0"].deviceID === "") {
+            streamDeck0 = openStreamDeck()
+        } else {
+            streamDeck0 = openStreamDeck(config.devices["0"].deviceID.toString(), {
+                useOriginalKeyOrder: true
+            })
+        }
+
+        if (typeof config.devices["0"].brightness !== undefined) {
+            streamDeck0.setBrightness(config.devices["0"].brightness.toString())
+        }
+
+        for (let key in config.devices["0"].keys) {
+            streamDeck0.fillColor(config.devices["0"].keys[key].toString(), config.devices["0"].keys[key].fillParam[0], config.devices["0"].keys[key].fillParam[1], config.devices["0"].keys[key].fillParam[2])
+        }
+
+        streamDeck0.on('down', keyIndex => {
+            console.log('key %d down', keyIndex)
+        })
+
+        streamDeck0.on('up', keyIndex => {
+            console.log('key %d up', keyIndex)
+        })
+
+        streamDeck0.on('error', error => {
+            console.error(error)
+        })
+
+
+    }
+}
+
+
+
+
 
 // Fired whenever an error is detected by the `node-hid` library.
 // Always add a listener for this event! If you don't, errors will be silently dropped.
-myStreamDeck.on('error', error => {
-    console.error(error)
-})
 
 // Fill the first button form the left in the first row with a solid red color. This is synchronous.
 myStreamDeck.fillColor(4, 255, 0, 0)
