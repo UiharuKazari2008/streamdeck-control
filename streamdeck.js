6588c1bc1bb45c538f8f35f005dc7bf97113e421
Yukimi Kazari <dev@9854.moe>
2020-08-31 21:35:40 -0400

    inital testing of sdc

--- streamdeck.js
+++ streamdeck.js
@@ -1,44 +1,62 @@
 const path = require('path')
-const { openStreamDeck, listStreamDecks } = require('elgato-stream-deck')
+const { openStreamDeck, listStreamDecks, getStreamDeckInfo } = require('elgato-stream-deck')
 
 const config = require('./config.json');
 
 console.log('Devices: ', listStreamDecks())
 
-let streamDeck0
+
 let streamDeck1
 let streamDeck2
 
 if (typeof config.devices["0"] !== undefined) {
-    if (typeof config.devices["0"].deviceID !== undefined) {
+
+    for (let devices in config.devices["0"].keys) {
+        let streamDeck // Init Device Object
+        let deviceInfo
+
         if (config.devices["0"].deviceID === "") {
-            streamDeck0 = openStreamDeck()
+            streamDeck = openStreamDeck() // No Device given? No Problem!
+            deviceInfo = getStreamDeckInfo()
+
         } else {
-            streamDeck0 = openStreamDeck(config.devices["0"].deviceID.toString(), {
-                useOriginalKeyOrder: true
-            })
+            // Given a specific ID
+            streamDeck = openStreamDeck(config.devices["0"].deviceID.toString())
+            deviceInfo = getStreamDeckInfo(config.devices["0"].deviceID.toString())
         }
+        // Print Device Info
+        console.log(deviceInfo)
 
+        // Reset keypad
+        streamDeck.clearAllKeys()
+
+        // Set Brightness if set
         if (typeof config.devices["0"].brightness !== undefined) {
-            streamDeck0.setBrightness(config.devices["0"].brightness.toString())
+            streamDeck.setBrightness(config.devices["0"].brightness.toString())
         }
 
+        // Fill keys from config
         for (let key in config.devices["0"].keys) {
             console.log(config.devices["0"].keys[key])
-            streamDeck0.fillColor(parseInt(key), config.devices["0"].keys[key].fillParam[0], config.devices["0"].keys[key].fillParam[1], config.devices["0"].keys[key].fillParam[2])
+            streamDeck.fillColor(parseInt(key), config.devices["0"].keys[key].fillParam[0], config.devices["0"].keys[key].fillParam[1], config.devices["0"].keys[key].fillParam[2])
         }
 
-        streamDeck0.on('down', keyIndex => {
+        // Press Key
+        streamDeck.on('down', keyIndex => {
             console.log('key %d down', keyIndex)
         })
-
-        streamDeck0.on('up', keyIndex => {
+        // Release Key
+        streamDeck.on('up', keyIndex => {
             console.log('key %d up', keyIndex)
         })
-
-        streamDeck0.on('error', error => {
+        // Error? What could possibly go wrong?
+        streamDeck.on('error', error => {
             console.error(error)
         })
+    }
+
+    if (typeof config.devices["0"].deviceID !== undefined) {
+
 
 
     }
